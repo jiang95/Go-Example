e4af23f0c9c177ecde00dbe758fe59ce2f3652e8
lingjun.jlj <jlj_vip98@qq.com>
2018-09-26 23:09:51 +0800

    fix part1

---
+++ part1/HelloWorld.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Hello World!")
+
+	//定义变量的三方式
+	var a int  = 10
+	var b  = 10
+	c := 10
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
